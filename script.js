45cf178ecd330a79d05633f1ea178bf450fd7296
84tigers <ahyska@gmail.com>
2018-09-27 12:13:19 -0400

    first commit for deliverable two

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+function myFunction() {
+var question = "Gimme a number!!!";
+var userNum = prompt(question);
+var total = 0;
+  for (var i = 0; i <= userNum; i++) {
+    total += i;
+  }
+alert('Ummmm, that would equal ' + total + ' thingies.');
+}
