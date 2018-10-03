29a4975a0e0272bb6e69b5c46844a24bb9617d32
84tigers <ahyska@gmail.com>
2018-10-03 12:18:29 -0400

    proper external js linking and do...while

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
+// Ex 3
 
-
-function myFunction(numberz) {
+// function myFunction(numberz) {
+function numberz() {
 var question = "Gimme a number!!!";
 var userNum = prompt(question);
 var total = 0;
@@ -10,3 +11,41 @@ var total = 0;
   }
 alert('Ummmm, that would equal ' + total + ' thingies.');
 }
+// }
+
+// Ex 2
+
+function wordz() {
+  var repeat, wordArr = [];
+
+      do {
+
+      wordArr.push(prompt("Enter a word: "));
+      repeat = prompt ("Do you want to enter another word: y/n");
+
+      } while (repeat !== 'n' && repeat !== 'N');
+
+  alert(wordArr.join("\n"));
+
+}
+
+// Ex 3
+
+var repeat, wordArr = [];
+/* var exNum = wordArr.length
+ */var exclaim = '!';
+ if (prompt(repeat) !== 'n' && repeat !== 'N');
+      while (repeat !== 'n' && repeat !== 'N' && wordArr !== false){
+          wordArr.push(prompt("Enter your name: "));
+              repeat = prompt ("Do you want to print your name : y/n");
+
+      }
+      alert('your name is ' + wordArr[0] + ' ' + (wordArr.length) * exclaim);
+/*       alert(wordArr.join('\n') + "\n" + repeat); */
+
+// Ex 4
+
+var timeOfDay = ['morning' 'noon' 'evening'];
+var foodSugg = ['eggs and toast.' 'a salad.' 'chicken and rice.'];
+var greeting = "Is it morning, noon, or evening? Please type one option given."
+prompt(greeting) 
